a2053fe36433e5a7ae8a27b16c2711df731aa814
rxdsqe <rxdsqe@gmail.com>
2025-10-04 20:47:15 +0500

    feat: style pages

--- shared/crypto/src/keychain.ts
+++ shared/crypto/src/keychain.ts
@@ -1,6 +1,9 @@
 import { generateMnemonic as scureGenerateMnemonic } from '@scure/bip39';
 import { wordlist } from '@scure/bip39/wordlists/english';
 
+// 128 = 12 words; 256 = 24 words;
+const MNEMONIC_STRENGTH = 128;
+
 export function generateMnemonic() {
-  return scureGenerateMnemonic(wordlist, 256);
+  return scureGenerateMnemonic(wordlist, MNEMONIC_STRENGTH);
 }
--- src/features/NewWallet/components/MnemonicDisplay.tsx
+++ src/features/NewWallet/components/MnemonicDisplay.tsx
@@ -12,7 +12,7 @@ export function MnemonicDisplay({ mnemonic }: MnemonicDisplayType) {
   const mnemonicSplit = mnemonic.split(' ');
 
   return (
-    <View className="flex-col flex-wrap gap-2 h-2/3">
+    <View className="flex-col flex-wrap w-[90%] h-2/3 border-4 border-custom_border rounded-5 bg-custom_complement">
       {mnemonicSplit.map((word, idx) => (
         <MnemonicWord key={word + idx} idx={idx + 1} word={word} />
       ))}
