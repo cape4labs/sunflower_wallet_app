6c03cd1878980f8f0392f41b2c9d68d78d39d644
rxdsqe <rxdsqe@gmail.com>
2025-10-05 22:58:35 +0500

    feat: wrapper component

--- src/features/NewWallet/screens/LogoScreen.tsx
+++ src/features/NewWallet/screens/LogoScreen.tsx
@@ -1,22 +1,25 @@
 import { Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from '../components/Button';
+import { Wrapper } from '../components/Wrapper';
 
 export function LogoScreen() {
   const navigation = useNavigation();
 
   return (
-    <View className="flex-1 justify-between items-center">
-      <View className="h-1/3 items-center justify-center gap-2">
-        <Text className="text-white text-2xl">Sunflower wallet</Text>
-        <Text className="text-white">Your BTCfi tool</Text>
-      </View>
+    <Wrapper>
+      <View className="flex-1 justify-between items-center">
+        <View className="items-center justify-center gap-2">
+          <Text className="text-white text-2xl">Sunflower wallet</Text>
+          <Text className="text-white">Your BTCfi tool</Text>
+        </View>
 
-      <Button
-        onPress={() => navigation.navigate('ImportCreateScreen')}
-        text={'Next'}
-        customStyle="mb-12"
-      />
-    </View>
+        <Button
+          onPress={() => navigation.navigate('ImportCreateScreen')}
+          text={'Next'}
+          customStyle="mb-12"
+        />
+      </View>
+    </Wrapper>
   );
 }
