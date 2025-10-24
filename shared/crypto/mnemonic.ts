e95d7cc2edf5a0109aca7ad981ede92c2c1bcf02
eqsdxr <rxdsqe@gmail.com>
2025-10-24 11:53:08 +0500

    refactor: generateMnemonic

--- shared/crypto/mnemonic.ts
+++ shared/crypto/mnemonic.ts
@@ -1,9 +1,6 @@
-import { generateMnemonic as scureGenerateMnemonic } from '@scure/bip39';
-import { wordlist } from '@scure/bip39/wordlists/english';
+import { AllowedKeyEntropyBits, randomSeedPhrase } from '@stacks/wallet-sdk';
 
 // 128 = 12 words; 256 = 24 words;
-const MNEMONIC_STRENGTH = 128;
-
-export function generateMnemonic() {
-  return scureGenerateMnemonic(wordlist, MNEMONIC_STRENGTH);
+export function generateMnemonic(keyEntropy: AllowedKeyEntropyBits = 128) {
+  return randomSeedPhrase(keyEntropy);
 }
