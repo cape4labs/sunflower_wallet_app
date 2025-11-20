5b2a15bcb59106f7098a513cbcc88c49e54de6c1
eqsdxr <eqs784.dev@proton.me>
2025-11-20 16:36:36 +0000

    fix: speed up wallet generation

--- polyfill.js
+++ polyfill.js
@@ -1,7 +1,17 @@
 import { Buffer } from 'buffer/';
 import process from 'process';
+
+// Fix `Error: crypto.getRandomValues must be defined react-native`
+// which is not available in React Native and used by @scure/bip39
+// https://www.npmjs.com/package/react-native-get-random-values
 import 'react-native-get-random-values';
+
 import { TextDecoder, TextEncoder } from 'text-encoding';
+
+// Speed up crypto operations
+// https://www.npmjs.com/package/react-native-quick-crypto?activeTab=readme
+import { install } from 'react-native-quick-crypto';
+install();
 
 global.process = process;
 global.Buffer = Buffer;
