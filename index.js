3472a6637b851fac4605f433b62dbf815d6c9498
eqsdxr <rxdsqe@gmail.com>
2025-11-20 17:11:55 +0000

    fix: speed up wallet generation (#35)

--- index.js
+++ index.js
@@ -2,13 +2,5 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 import './polyfill';
-import { Crypto } from '@peculiar/webcrypto';
-import 'react-native-get-random-values';
-
-// Fix `Error: crypto.getRandomValues must be defined react-native`
-// which is not available in React Native and used by @scure/bip39
-// https://www.npmjs.com/package/react-native-get-random-values
-
-Object.assign(global.crypto, new Crypto());
 
 AppRegistry.registerComponent(appName, () => App);
