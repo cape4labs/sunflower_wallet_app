4fce691b45b3eedd7b33366e475535c54519277e
isgin01 <isgin01@gmail.com>
2026-01-18 01:32:02 +0500

    chore(tests): make some modules being transpiled explicitly for jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  // Transpile these modules explicitly since they cause issues when used with jest and are
+  // located in node_modules which is ignored by default by jest
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@scure|@noble)/)',
+  ],
   setupFiles: ['./jest.setup.js'],
 };
