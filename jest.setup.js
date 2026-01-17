aad2b37b554318c050d381094fbf487c54235768
isgin01 <isgin01@gmail.com>
2026-01-18 01:30:29 +0500

    chore(tests): mock native clipboard module

---
+++ jest.setup.js
@@ -0,0 +1,3 @@
+// TODO: perhaps, the mock below can be used somehow
+// export * from '@react-native-clipboard/clipboard/jest/clipboard-mock.js';
+jest.mock('@react-native-clipboard/clipboard', () => {});
